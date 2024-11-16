60489607a9216a767af88444a8901cf1472c9dec
chuy <chris2kus31@gmail.com>
2024-11-16 06:00:17 -0600

    adding basic hello world page

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-      Hello world
+    <div class="todo-container">
+      <div class="todo-widget">
+        Hello world
+      </div>
     </div>
   </template>
+  
+  <style scoped>
+  .todo-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+  
+  .todo-widget {
+    padding: 20px;
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    text-align: center;
+    background-color: #f9f9f9;
+  }
+  </style>
   
